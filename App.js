8fe46f5b3cf0da33e7f488e9c62e986a764666bc
fachri-literasia <fachri.amin@literasia.co.id>
2021-11-22 20:06:59 +0700

    init project

---
+++ App.js
@@ -0,0 +1,168 @@
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
+
+import RtcEngine, {
+  RtcLocalView,
+  RtcRemoteView,
+  VideoRenderMode,
+} from 'react-native-agora';
+import requestCameraAndAudioPermission from './components/Permission';
+import styles from './components/style';
+import axios from 'axios';
+
+const App = () => {
+  let _engine;
+  const [appId, setAppId] = useState('c35af1410df9462a88e14d7334508b1b');
+  const [token, setToken] = useState("006c35af1410df9462a88e14d7334508b1bIADqk7+HMBQQkkfO8iqS52FfK/8jn4ZTMHDPXd/W9NCLD92N+lQAAAAAEADsTG0XTNaYYQEAAQBN1phh");
+  const [channelName, setChannelName] = useState('fachri-call-2');
+  const [joinSucceed, setJoinSucceed] = useState(false);
+  const [peerIds, setPeerIds] = useState([]);
+  const [userId, setUserId] = useState(Math.floor(Math.random()*100000));
+
+  init = async () => {
+    // await axios.get(`https://express-agora.herokuapp.com/generate-token/${userId}/fachri-call-2`)
+    //   .then((res) => {
+    //     console.log('ini token', res.data);
+    //     setToken(res.data.token);
+    //     setUserId(res.data.uid);
+    //     console.log('ini token',token);
+    //   })
+    //   .catch((err) => {
+    //     console.log('terjadi kesalahan');
+    //   })
+
+    _engine = await RtcEngine.create(appId);
+    console.log('init',_engine);
+    await _engine.enableVideo();
+
+    _engine.addListener('Warning', (warn) => {
+      console.log('Warning', warn);
+    });
+
+    _engine.addListener('Error', (err) => {
+      console.log('Error', err);
+    });
+
+    _engine.addListener('UserJoined', (uid, elapsed) => {
+      console.log('uid', uid);
+      console.log('UserJoined', uid, elapsed);
+      // If new user
+      if (peerIds.indexOf(uid) === -1) {
+        setPeerIds([...peerIds, uid]);
+      }
+    });
+
+    _engine.addListener('UserOffline', (uid, reason) => {
+      console.log('UserOffline', uid, reason);
+      setPeerIds(peerIds.filter((id) => id !== uid));
+    });
+
+    // If Local user joins RTC channel
+    _engine.addListener('JoinChannelSuccess', (channel, uid, elapsed) => {
+      console.log('JoinChannelSuccess', channel, uid, elapsed);
+      // Set state variable to true
+      setJoinSucceed(true);
+    });
+  };
+
+  useEffect(() => {
+    // axios.get(`https://express-agora.herokuapp.com/generate-token/${userId}/fachri-call-2`)
+    //   .then((res) => {
+    //     console.log('ini token', res.data);
+    //     setToken(res.data.token);
+    //     setUserId(res.data.uid);
+    //     console.log('ini token',token);
+    //   })
+    //   .catch((err) => {
+    //     console.log('terjadi kesalahan');
+    //   })
+
+    requestCameraAndAudioPermission().then(() => {
+      console.log('requested!');
+    });
+
+  }, []);
+
+  startCall = async () => {
+    await init();
+
+    console.log('start call',_engine);
+    // Join Channel using null token and channel name
+    await _engine?.joinChannel(
+      token,
+      channelName,
+      userId,
+      0
+    );
+  };
+
+  endCall = async () => {
+    await _engine?.leaveChannel();
+    setPeerIds([]);
+    setJoinSucceed(false);
+  };
+
+  _renderRemoteVideos = () => {
+    return (
+      <ScrollView
+        style={styles.remoteContainer}
+        contentContainerStyle={{ paddingHorizontal: 2.5 }}
+        horizontal={true}
+      >
+        {peerIds.map((value) => {
+          return (
+            <RtcRemoteView.SurfaceView
+              style={styles.remote}
+              uid={value}
+              channelId={channelName}
+              renderMode={VideoRenderMode.Hidden}
+              zOrderMediaOverlay={true}
+            />
+          );
+        })}
+      </ScrollView>
+    );
+  };
+
+  _renderVideos = () => {
+    return joinSucceed ? (
+      <View style={styles.fullView}>
+        <RtcLocalView.SurfaceView
+          style={styles.max}
+          channelId={channelName}
+          renderMode={VideoRenderMode.Hidden}
+        />
+        {_renderRemoteVideos()}
+      </View>
+    ) : null;
+  };
+
+
+  return (
+    <View style={styles.max}>
+        <View style={styles.max}>
+          <View style={styles.buttonHolder}>
+            <TouchableOpacity onPress={() => startCall()} style={styles.button}>
+              <Text style={styles.buttonText}> Start Call</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => endCall()} style={styles.button}>
+              <Text style={styles.buttonText}> End Call </Text>
+            </TouchableOpacity>
+          </View>
+          {_renderVideos()}
+        </View>
+      </View>
+  );
+};
+
+export default App;
